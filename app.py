fb4f6f41bebbe3b5628df177d4416dd6572dda77
Collincehals <129850364+Collincehals@users.noreply.github.com>
2023-06-10 07:09:11 +0000

    login page added

--- app.py
+++ app.py
@@ -62,5 +62,12 @@ def submit_form():
     
     return 'Form submitted successfully'
 
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+
+
+  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
