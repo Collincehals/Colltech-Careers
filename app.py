0be6bfcb4ef3cb2b8135ee2018e677eb988eb2b8
Collincehals <129850364+Collincehals@users.noreply.github.com>
2023-06-13 16:20:11 +0000

    more adjustments

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from database import load_jobs_from_db, load_job_from_db, add_application_to_db
+from email_sender import send_confirmation_email
+
 app = Flask(__name__)
 
 #Routes here#
@@ -48,72 +50,6 @@ def fill_form(id):
   return render_template('applicform.html',
                          job=job)
 
-
-
-
-from mailjet_rest import Client
-
-def send_confirmation_email(job_title, applicant_email, applicant_name,last_name, country):
-    api_key = '44c0aadab815085c5e43381feea607cc'
-    api_secret = 'dddf503912dfa3a5fdacea7a414db788'
-
-    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
-
-    email_data = {
-        'Messages': [
-            {
-                'From': {
-                    'Email':'odhiambocollince702@gmail.com',
-                    'Name': 'Colltech Careers'
-                },
-                'To': [
-                    {
-                        'Email': applicant_email
-                    }
-                ],
-                'Subject': 'Application Confirmation',
-                'HTMLPart': '''
-                    <html>
-                        <body>
-                            <h3>Subject: Application Confirmation - {job_title} Role</h3>
-                            <p>
-                                Dear {applicant_name},
-                            </p>
-                            <p>
-                                Thank you for applying for the position of {job_title} role. We have received your application and would like to confirm its successful submission.
-                            </p>
-                            <h4>Application Details:</h4>    
-                            <ul>
-                                <li>Job Title: {job_title}</li>
-                                <li>Applicant Name: {applicant_name} {last_name}</li>
-                                <li>Country: {country}</li>
-                                <li>Email: {applicant_email}</li>
-                            </ul>
-                            <p>
-                                Our team will carefully review your application and get back to you soon regarding the next steps.<br>
-                                If you have any questions, feel free to reach out to our HR department at colltechcareers@gmail.com or through our toll-free landline number at +010200100.<br>
-                                Thank you for your Job application through Colltech Careers website. <br>
-                                Best of luck with your application!<br></br>
-                            </p>
-                            <p>
-                                Best regards,<br></br>
-                                Hals,<br></br>
-                                Talents Manager,<br></br>
-                                Colltech Careers ltd.
-                            </p>
-                        </body>
-                    </html>
-                '''.format(applicant_name=applicant_name, applicant_email=applicant_email, job_title=job_title,last_name=last_name, country=country)
-            }
-        ]
-    }
-
-    result = mailjet.send.create(data=email_data)
-
-    if result.status_code == 200:
-        print('Confirmation email sent successfully.')
-    else:
-        print('Failed to send confirmation email.')
 
 # Form Submission here
 @app.route("/form/<id>/submit", methods=["POST"])
